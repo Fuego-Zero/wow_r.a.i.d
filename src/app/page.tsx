bf82aee96efa83ad8010526f76cd802e80bd0635
Fuego <FuegoZero35@gmail.com>
2025-03-06 17:34:50 +0800

    在 Home 组件中添加状态管理，传递数据到 AppHeader 和 RaidContent 组件以增强数据交互

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,24 @@ import zhCN from "antd/locale/zh_CN";
 import AppHeader from "./components/AppHeader";
 import "@ant-design/v5-patch-for-react-19";
 import RaidContent from "./components/RaidContent";
+import { useState } from "react";
+import { Data } from "./types";
 
 const { Header, Content } = Layout;
 
 export default function Home() {
+  const [data, setData] = useState<Data>([]);
+
   return (
     <StyleProvider layer>
       <ConfigProvider locale={zhCN} theme={{ algorithm: theme.darkAlgorithm }}>
         <App>
           <Layout className="h-[100vh] flex overflow-auto">
             <Header className="sticky top-0 z-10 w-full">
-              <AppHeader />
+              <AppHeader setData={setData} />
             </Header>
             <Content>
-              <RaidContent />
+              <RaidContent data={data} />
             </Content>
           </Layout>
         </App>
