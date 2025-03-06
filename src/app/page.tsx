94d6cf818495ffc18ba1fea56908b5e13885e8b9
Fuego <FuegoZero35@gmail.com>
2025-03-06 16:47:03 +0800

    将 Layout 组件包裹在 App 组件中，以提升应用的结构和一致性

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { StyleProvider } from "@ant-design/cssinjs";
-import { ConfigProvider, theme, Layout } from "antd";
+import { ConfigProvider, theme, Layout, App } from "antd";
 import zhCN from "antd/locale/zh_CN";
 import AppHeader from "./components/AppHeader";
 import "@ant-design/v5-patch-for-react-19";
@@ -13,14 +13,16 @@ export default function Home() {
   return (
     <StyleProvider layer>
       <ConfigProvider locale={zhCN} theme={{ algorithm: theme.darkAlgorithm }}>
-        <Layout className="h-[100vh] flex overflow-auto">
-          <Header className="sticky top-0 z-10 w-full">
-            <AppHeader />
-          </Header>
-          <Content>
-            <RaidContent />
-          </Content>
-        </Layout>
+        <App>
+          <Layout className="h-[100vh] flex overflow-auto">
+            <Header className="sticky top-0 z-10 w-full">
+              <AppHeader />
+            </Header>
+            <Content>
+              <RaidContent />
+            </Content>
+          </Layout>
+        </App>
       </ConfigProvider>
     </StyleProvider>
   );
