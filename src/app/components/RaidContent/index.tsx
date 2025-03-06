b7e8fcad69632c742fcbf0c126b4c9f4fd22594e
Fuego <FuegoZero35@gmail.com>
2025-03-06 17:34:57 +0800

    在 RaidContent 组件中添加数据传递功能，使用 props 接收 Data 类型的数据并渲染 RaidCard 组件

--- src/app/components/RaidContent/index.tsx
+++ src/app/components/RaidContent/index.tsx
@@ -1,13 +1,16 @@
 import { Col, Row } from "antd";
 import React from "react";
 import RaidCard from "./components/RaidCard";
+import { Data } from "@/app/types";
 
-function RaidContent() {
+function RaidContent(props: { data: Data }) {
+  const { data } = props;
+
   return (
     <Row gutter={[16, 8]} className="!mx-0 mt-5">
-      {Array.from({ length: 5 }).map((_, index) => (
+      {data.map((item, index) => (
         <Col xs={24} lg={12} key={index}>
-          <RaidCard />
+          <RaidCard data={item} />
         </Col>
       ))}
     </Row>
