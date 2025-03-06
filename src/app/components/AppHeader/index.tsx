11553fa7b1ad21f8c019f42720b372bc2a5cf45e
Fuego <FuegoZero35@gmail.com>
2025-03-06 17:35:21 +0800

    在 AppHeader 组件中添加文件上传功能，支持选择和提交名单文件，并通过 props 更新数据

--- src/app/components/AppHeader/index.tsx
+++ src/app/components/AppHeader/index.tsx
@@ -1,17 +1,59 @@
-import { Button } from "antd";
-import React from "react";
+"use client";
 
-function AppHeader() {
+import { Data } from "@/app/types";
+import { App, Button } from "antd";
+import axios from "axios";
+import React, { useState } from "react";
+
+function AppHeader(props: {
+  setData: React.Dispatch<React.SetStateAction<Data>>;
+}) {
+  const { setData } = props;
+  const [file, setFile] = useState<File>();
+  const { message } = App.useApp();
+
+  function onUpload() {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = ".xlsx,.xls,.csv"; // 限制文件类型
+    input.style.display = "none";
+
+    input.onchange = (e) => {
+      const target = e.target as HTMLInputElement;
+      if (!target.files || target.files.length === 0) return;
+
+      const selectedFile = target.files[0];
+      setFile(selectedFile);
+
+      message.success(`文件 "${selectedFile.name}" 已选择`);
+
+      document.body.removeChild(input);
+    };
+
+    document.body.appendChild(input);
+    input.click();
+  }
+
+  async function submit() {
+    if (!file) return message.error("请先上传名单");
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await axios.post("/api/roster", formData);
+    setData(res.data);
+  }
+
   return (
     <div className="flex">
       <h1 className="flex-1">
         <span className="text-2xl text-amber-50">WOW R.A.I.D</span>
         <span className="ml-2">(Roster Auto-Integrated Deployment)</span>
       </h1>
-
+      <div className="mr-5">{file && file.name}</div>
       <div>
-        <Button>上传名单</Button>
-        <Button className="ml-5" type="primary">
+        <Button onClick={onUpload}>上传名单</Button>
+        <Button className="ml-5" type="primary" onClick={submit}>
           自动分配
         </Button>
       </div>
