e1ee7cf7e09523a16efc778d76dce110de5b4389
Fuego <FuegoZero35@gmail.com>
2025-03-06 22:36:29 +0800

    在 RaidContent 组件中添加 delPlayer 属性，并将其传递给 RaidCard 组件

--- src/app/components/RaidContent/index.tsx
+++ src/app/components/RaidContent/index.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import RaidCard from "./components/RaidCard";
 import { Data } from "@/app/types";
 
-function RaidContent(props: { data: Data }) {
-  const { data } = props;
+function RaidContent(props: {
+  data: Data;
+  delPlayer: (time: string, index: number) => void;
+}) {
+  const { data, ...rest } = props;
 
   return (
     <Row gutter={[16, 8]} className="!mx-0 py-5">
       {data.map((item, index) => (
         <Col xs={24} lg={12} key={index}>
-          <RaidCard data={item} />
+          <RaidCard data={item} {...rest} />
         </Col>
       ))}
     </Row>
