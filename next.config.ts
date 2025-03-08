89c24d6d0be2f9d055c204b327dbe6d16b406106
Fuego <FuegoZero35@gmail.com>
2025-03-09 03:05:59 +0800

    优化 next.config.ts，调整开发环境下的API反向代理配置逻辑

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: process.env.NODE_ENV === "production" ? "export" : undefined,
+};
 
-  // 开发环境下的API反向代理配置
-  async rewrites() {
-    if (process.env.NODE_ENV !== "development") {
-      return [];
-    }
-
+if (process.env.NODE_ENV === "development") {
+  nextConfig.rewrites = async () => {
     return [
       {
         source: "/api/:path*", // 只匹配API路径
         destination: "http://127.0.0.1:7000/api/:path*",
       },
     ];
-  },
-};
+  };
+}
 
 export default nextConfig;
