d8ea69f811bd5fed37effc02b8353330bbbb20bb
Fuego <FuegoZero35@gmail.com>
2025-03-12 17:14:04 +0800

    添加对 API2 路径的支持，配置新的代理目标

--- web/next.config.ts
+++ web/next.config.ts
@@ -12,6 +12,10 @@ if (process.env.NODE_ENV === "development") {
         source: "/api/:path*", // 只匹配API路径
         destination: "http://127.0.0.1:7000/api/:path*",
       },
+      {
+        source: "/api2/:path*", // 只匹配API2路径
+        destination: "http://127.0.0.1:34567/api/:path*",
+      },
     ];
   };
 }
