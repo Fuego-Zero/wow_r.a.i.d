aec4e24b0a551837a6d19f5fa3b090a4c0350edc
Fuego <FuegoZero35@gmail.com>
2025-03-13 08:58:01 +0800

    添加登录组件以支持用户认证功能

---
+++ web/src/app/player/components/Login.tsx
@@ -0,0 +1,58 @@
+import { App, Button, Form, FormProps, Input, Row } from "antd";
+import React from "react";
+import { useAuth } from "../context";
+import { isBizException } from "@yfsdk/web-basic-library";
+
+type FieldType = {
+  account: string;
+  password: string;
+};
+
+function Login() {
+  const { message } = App.useApp();
+  const { login } = useAuth();
+
+  const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
+    try {
+      await login(values);
+    } catch (error) {
+      if (isBizException(error)) {
+        message.error(error.message);
+      }
+    }
+  };
+
+  return (
+    <Row justify="center" className="mt-[20vh]">
+      <Form
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType>
+          label="账号"
+          name="account"
+          rules={[{ required: true, message: "请输入账号!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="密码"
+          name="password"
+          rules={[{ required: true, message: "请输入密码!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item label={null}>
+          <Button type="primary" htmlType="submit" block>
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+    </Row>
+  );
+}
+
+export default Login;
