d47e87d7b06ed0a05e0602f7f4c67364658c3f39
Fuego <FuegoZero35@gmail.com>
2025-03-13 14:24:06 +0800

    添加全局错误处理程序以支持数据库验证错误的处理

--- server_node/src/middleware/globalErrorHandler.ts
+++ server_node/src/middleware/globalErrorHandler.ts
@@ -1,11 +1,16 @@
 import { isBizException } from '@yfsdk/web-basic-library';
 import { Middleware } from 'koa';
 
+function isValidationError(error: any): error is { message: string } {
+  return error.name === 'ValidationError';
+}
+
 const globalErrorMiddleware: Middleware = async (ctx, next) => {
   try {
     await next();
   } catch (error) {
     if (isBizException(error)) return ctx.bizError(error.message);
+    if (isValidationError(error)) return ctx.bizError(error.message);
     console.error('Global Error Handler:', error);
     ctx.apiError((error as Error).message);
   }
