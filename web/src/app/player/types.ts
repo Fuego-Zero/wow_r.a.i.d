9a87be673272d26366606b3805363ef30680fbdf
Fuego <FuegoZero35@gmail.com>
2025-03-13 16:05:11 +0800

    添加用户信息和角色信息类型定义

---
+++ web/src/app/player/types.ts
@@ -0,0 +1,36 @@
+import { RoleClasses, TalentType } from "../components/RaidContent/constant";
+
+export type UserInfo = {
+  /**
+   * 用户名
+   */
+  user_name: string;
+  /**
+   * 微信名
+   */
+  wechat_name: string;
+  /**
+   * 游戏时间
+   */
+  play_time: string[];
+  /**
+   * token
+   */
+  token: string;
+};
+
+export type RoleInfo = {
+  /**
+   * 角色名
+   */
+  role_name: string;
+  /**
+   * 角色职业
+   */
+  classes: RoleClasses;
+  /**
+   * 角色天赋
+   */
+  talent: TalentType[];
+  id: string;
+};
