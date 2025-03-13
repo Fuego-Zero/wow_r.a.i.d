ba1427bcf5b79d4d0706248d845ba5dec2dc98eb
Fuego <FuegoZero35@gmail.com>
2025-03-13 16:05:06 +0800

    添加用户信息获取、登录、角色绑定及获取所有角色的API接口

---
+++ web/src/app/player/api.ts
@@ -0,0 +1,26 @@
+import http from "./http";
+import { RoleInfo, UserInfo } from "./types";
+
+export function getUserInfo(): Promise<UserInfo> {
+  return http.get("/user_info");
+}
+
+export function login(params: {
+  account: string;
+  password: string;
+}): Promise<UserInfo> {
+  return http.post("/login", params);
+}
+
+export function bindRole(params: {
+  name: string;
+  classes: string;
+  talent: string[];
+}): Promise<RoleInfo> {
+  const { name, ...rest } = params;
+  return http.post("/bind_role", { role_name: name, ...rest });
+}
+
+export function getAllRole(): Promise<RoleInfo[]> {
+  return http.get("/get_all_role");
+}
