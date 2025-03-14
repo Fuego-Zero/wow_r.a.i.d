8578f02394dfb0737bc2349ec126c020850e6f12
Fuego <FuegoZero35@gmail.com>
2025-03-15 04:08:41 +0800

    更新 API 基础路径为 /api_node，并调整 MongoDB 连接地址配置

--- server_node/src/config/index.ts
+++ server_node/src/config/index.ts
@@ -5,11 +5,12 @@ export const signKey = 'OJuYmvBgTpSMK4AvuOAG4vMhtDoJFBzzTsey1oVwqp6pBy804zJOA978
 export const adminKey =
   'om4lyJOQbIZrZfo9nWAD8JWZiS5e7N8TUH2T8SAmfJuElp5Jf48ASsn5YfCCy4FlvDgoQ0s3mm3OJR9u4hkIErSMBtVcF8xEX6evJOA9W8XTM0h4bhtdrd4zBCb02bz1';
 
-export const BASE_PREFIX = '/api';
+export const BASE_PREFIX = '/api_node';
 export const ROUTER_HEALTH = '/health';
 export const ROUTER_VERSION = '/version';
 
 const PROJECT_NAME = 'wow_raid';
-const MONGODB_URL = process.env.NODE_ENV === 'production' ? '172.22.0.2' : '127.0.0.1';
+// const MONGODB_URL = process.env.NODE_ENV === 'production' ? 'mongoDB' : '144.34.226.99';
+const MONGODB_URL = process.env.NODE_ENV === 'production' ? 'mongoDB' : '127.0.0.1';
 
 export const DB_URI = `mongodb://${MONGODB_URL}:27017/${PROJECT_NAME}`;
