7a277db56d1ccd1d2ef5a66b386ea594d3ead1a7
Fuego <FuegoZero35@gmail.com>
2025-03-17 20:44:53 +0800

    在RaidContent组件中添加空数据提示，若当前CD排班表未发布则显示相应信息

--- web/src/app/raid-roster/component/RaidContent/index.tsx
+++ web/src/app/raid-roster/component/RaidContent/index.tsx
@@ -11,15 +11,23 @@ function RaidContent(
 ) {
   const { data, ...rest } = props;
 
-  return (
-    <Row gutter={[16, 8]} className="!mx-0 py-5">
-      {data.map((item, index) => (
-        <Col md={24} xxl={12} key={index}>
-          <RaidCard data={item} {...rest} />
-        </Col>
-      ))}
-    </Row>
-  );
+  if (data.length === 0) {
+    return (
+      <div className="text-center text-amber-50 mt-[30vh] text-4xl">
+        当前CD排班表还未发布，尽情期待！
+      </div>
+    );
+  } else {
+    return (
+      <Row gutter={[16, 8]} className="!mx-0 py-5">
+        {data.map((item, index) => (
+          <Col md={24} xxl={12} key={index}>
+            <RaidCard data={item} {...rest} />
+          </Col>
+        ))}
+      </Row>
+    );
+  }
 }
 
 export default memo(RaidContent);
