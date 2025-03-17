8ffc00899ab2229cb05a5881dade1dadbf2ea386
Fuego <FuegoZero35@gmail.com>
2025-03-17 15:22:37 +0800

    重构 Home 组件，移除不必要的样式提供者，添加 BaseProvider 以简化结构

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { StyleProvider } from "@ant-design/cssinjs";
-import { ConfigProvider, theme, Layout, App, message } from "antd";
-import zhCN from "antd/locale/zh_CN";
+import { Layout, message } from "antd";
 import "@ant-design/v5-patch-for-react-19";
 import classNames from "classnames";
 import RaidContent from "./raid-roster/component/RaidContent";
@@ -11,8 +9,9 @@ import { PlayersData, RaidData } from "./raid-roster/types";
 import { formatRaidDataData } from "./utils";
 import { getPublishedSchedule } from "./api";
 import { isBizException } from "@yfsdk/web-basic-library";
+import BaseProvider from "./components/common/BaseProvider";
 
-export default function Home() {
+function HomeContent() {
   const [playersData, setPlayersData] = useState<PlayersData>([]);
 
   const raidData = useMemo<RaidData>(() => {
@@ -32,27 +31,25 @@ export default function Home() {
   useEffect(() => {
     onLoadPlayersData();
   }, []);
-
   return (
-    <StyleProvider layer>
-      <ConfigProvider locale={zhCN} theme={{ algorithm: theme.darkAlgorithm }}>
-        <App>
-          <Layout
-            className={classNames("h-[100vh] flex overflow-auto")}
-            // ref={el}
-          >
-            <Layout.Header className="sticky top-0 z-10 w-full">
-              <h1 className="flex-1">
-                <span className="text-2xl text-amber-50">轻风之语</span>
-                <span className="ml-2">(副本活动排班表)</span>
-              </h1>
-            </Layout.Header>
-            <Layout.Content>
-              <RaidContent data={raidData} displayMode={true} />
-            </Layout.Content>
-          </Layout>
-        </App>
-      </ConfigProvider>
-    </StyleProvider>
+    <Layout className={classNames("h-[100vh] flex overflow-auto")}>
+      <Layout.Header className="sticky top-0 z-10 w-full">
+        <h1 className="flex-1">
+          <span className="text-2xl text-amber-50">轻风之语</span>
+          <span className="ml-2">(副本活动排班表)</span>
+        </h1>
+      </Layout.Header>
+      <Layout.Content>
+        <RaidContent data={raidData} displayMode={true} />
+      </Layout.Content>
+    </Layout>
+  );
+}
+
+export default function Home() {
+  return (
+    <BaseProvider>
+      <HomeContent />
+    </BaseProvider>
   );
 }
