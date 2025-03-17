dec4b4b5a7bd4bf55d6f0ebe7825152d62ee8cce
Fuego <FuegoZero35@gmail.com>
2025-03-17 15:00:55 +0800

    为 Nameplate 组件添加 WCL 排名显示功能，支持自定义类名

--- web/src/app/player/components/Nameplate.tsx
+++ web/src/app/player/components/Nameplate.tsx
@@ -1,21 +1,34 @@
 import Actor from "@/app/components/Actor";
 import Players from "@/app/components/Players";
 import { RoleClasses, TalentType } from "@/app/constant";
+import classNames from "classnames";
 import React from "react";
+import { useAppConfig } from "../context/appConfigContext";
+import WCL from "./WCL";
 
 type Props = {
   classes: RoleClasses;
   talent: TalentType[];
   name: string;
+  className?: string;
 };
 
 function Nameplate(props: Props) {
-  const { classes, talent, name } = props;
+  const { WCLRanksMap } = useAppConfig();
+  const { classes, talent, name, className } = props;
 
   return (
-    <div className="flex items-center space-x-2">
+    <div className={classNames("flex items-center mr-2", className)}>
       <Actor actor={talent} />
       <Players classes={classes}>{name}</Players>
+      {talent.map((item) => {
+        return (
+          <WCL
+            key={item}
+            rank={WCLRanksMap.get(name + item)?.average_rank_percent}
+          />
+        );
+      })}
     </div>
   );
 }
