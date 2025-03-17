21cd74342a8859c5c075828eaeb87815fa2f8247
Fuego <FuegoZero35@gmail.com>
2025-03-17 15:59:27 +0800

    为 HomeContent 组件添加个人中心按钮，优化用户导航体验

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Layout, message } from "antd";
+import { Button, Layout, message } from "antd";
 import "@ant-design/v5-patch-for-react-19";
 import classNames from "classnames";
 import RaidContent from "./raid-roster/component/RaidContent";
@@ -10,6 +10,7 @@ import { formatRaidDataData } from "./utils";
 import { getPublishedSchedule } from "./api";
 import { isBizException } from "@yfsdk/web-basic-library";
 import BaseProvider from "./components/common/BaseProvider";
+import { useRouter } from "next/navigation";
 
 function HomeContent() {
   const [playersData, setPlayersData] = useState<PlayersData>([]);
@@ -31,13 +32,25 @@ function HomeContent() {
   useEffect(() => {
     onLoadPlayersData();
   }, []);
+
+  const router = useRouter();
+
   return (
     <Layout className={classNames("h-[100vh] flex overflow-auto")}>
-      <Layout.Header className="sticky top-0 z-10 w-full">
+      <Layout.Header className="sticky top-0 z-10 w-full flex items-center">
         <h1 className="flex-1">
           <span className="text-2xl text-amber-50">轻风之语</span>
           <span className="ml-2">(副本活动排班表)</span>
         </h1>
+        <Button
+          type="link"
+          className="ml-2"
+          onClick={() => {
+            router.push("/player");
+          }}
+        >
+          个人中心
+        </Button>
       </Layout.Header>
       <Layout.Content>
         <RaidContent data={raidData} displayMode={true} />
