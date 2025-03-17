a9b263ec3cad5a7a34e34ca9912aabe9d110c977
Fuego <FuegoZero35@gmail.com>
2025-03-17 10:28:26 +0800

    添加用户访问验证功能，检查用户是否为管理员

--- server_node/src/utils/user.ts
+++ server_node/src/utils/user.ts
@@ -3,6 +3,12 @@ import { BizException } from '@yfsdk/web-basic-library';
 import User from '../models/User';
 import { UserId } from '../types';
 
+/**
+ * 验证用户是否为管理员
+ *
+ * @param userId
+ * @returns
+ */
 export async function validateUserAccess(userId: UserId): Promise<boolean> {
   const user = await User.findOne({ _id: userId });
   if (!user) throw new BizException('用户不存在');
