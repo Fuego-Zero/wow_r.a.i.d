20625d9efa4896d2e0b9aa941ccef561109c41f3
Fuego <FuegoZero35@gmail.com>
2025-03-18 18:14:35 +0800

    在主页中引入 AppMenu 组件，移除个人中心按钮，优化布局

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Layout, message } from "antd";
+import { Layout, message } from "antd";
 import "@ant-design/v5-patch-for-react-19";
 import classNames from "classnames";
 import RaidContent from "./raid-roster/component/RaidContent";
@@ -10,8 +10,8 @@ import { formatRaidDataData } from "./utils";
 import { getPublishedSchedule } from "./api";
 import { isBizException } from "@yfsdk/web-basic-library";
 import BaseProvider from "./components/common/BaseProvider";
-import { useRouter } from "next/navigation";
 import Welcome from "./components/Welcome";
+import AppMenu from "./components/Menu";
 
 function HomeContent() {
   const [playersData, setPlayersData] = useState<PlayersData>([]);
@@ -34,24 +34,14 @@ function HomeContent() {
     onLoadPlayersData();
   }, []);
 
-  const router = useRouter();
-
   return (
     <Layout className={classNames("h-[100vh] flex overflow-auto")}>
       <Layout.Header className="sticky top-0 z-10 w-full flex items-center">
         <h1 className="flex-1">
           <span className="text-2xl text-amber-50">轻风之语</span>
-          <span className="ml-2">(副本活动排班表)</span>
+          <span className="ml-2">副本活动排班表</span>
         </h1>
-        <Button
-          type="link"
-          className="ml-2"
-          onClick={() => {
-            router.push("/player");
-          }}
-        >
-          个人中心
-        </Button>
+        <AppMenu />
       </Layout.Header>
       <Layout.Content>
         <RaidContent data={raidData} displayMode={true} />
