5f0d99027d384f5a9df3a5370fa9b66c21b41df0
Fuego <FuegoZero35@gmail.com>
2025-03-18 18:14:39 +0800

    新增 getWClColor 函数，根据排名返回对应的颜色值

--- web/src/app/utils/index.ts
+++ web/src/app/utils/index.ts
@@ -80,3 +80,13 @@ export function formatRaidDataData(playersData: PlayersData): RaidData {
 
   return data;
 }
+
+export function getWClColor(rank: number) {
+  if (rank < 25) return "#666666";
+  if (rank < 50) return "#1eff00";
+  if (rank < 75) return "#0070ff";
+  if (rank < 95) return "#a335ee";
+  if (rank < 99) return "#ff8000";
+  if (rank < 100) return "#e268a8";
+  if (rank === 100) return "#e5cc80";
+}
