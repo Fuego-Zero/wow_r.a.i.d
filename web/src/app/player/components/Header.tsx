57054693364ab93095efcde3e8664ce2a31d8894
Fuego <FuegoZero35@gmail.com>
2025-03-19 14:25:49 +0800

    重构 Header 组件，移除旧的用户菜单，新增公共菜单和用户菜单组件

--- web/src/app/player/components/Header.tsx
+++ web/src/app/player/components/Header.tsx
@@ -1,82 +1,15 @@
-import { MenuOutlined } from "@ant-design/icons";
-import { Button, Dropdown, MenuProps } from "antd";
 import React from "react";
 import { useAuth } from "../context/authContext";
-import useChangePassword from "../hooks/useChangePassword";
-import useChangeUserinfo from "../hooks/useChangeUserInfo";
-import { useRouter } from "next/navigation";
-import AppMenu from "@/app/components/Menu";
+import AppPublicMenu from "@/app/components/AppPublicMenu";
+import AppUserMenu from "@/app/components/AppUserMenu";
 
 export const Header = () => {
-  const { isLogin, isAdmin, logout } = useAuth();
-  const [openChangePassword, changePasswordHolder] = useChangePassword();
-  const [openChangeUserinfo, changeUserinfoHolder] = useChangeUserinfo();
-  const router = useRouter();
-
-  const items: MenuProps["items"] = [
-    {
-      key: "1",
-      label: "编辑账号",
-      onClick: () => {
-        openChangeUserinfo();
-      },
-    },
-    {
-      key: "2",
-      label: "修改密码",
-      onClick: () => {
-        openChangePassword();
-      },
-    },
-    {
-      key: "3",
-      danger: true,
-      label: "退出登录",
-      onClick: () => {
-        logout();
-      },
-    },
-  ];
-
-  if (isAdmin) {
-    items.unshift({
-      key: "0",
-      label: "管理后台",
-      onClick: () => {
-        router.push("/manage");
-      },
-    });
-    items.unshift({
-      key: "-1",
-      label: "团队编排",
-      onClick: () => {
-        router.push("/raid-roster");
-      },
-    });
-  }
+  const { isLogin } = useAuth();
 
   return (
     <div className="flex items-center h-full">
       <span className="flex-1 text-xl">个人中心</span>
-      {isLogin ? (
-        <>
-          <Button
-            type="link"
-            onClick={() => {
-              router.push("/");
-            }}
-          >
-            首页
-          </Button>
-          <Dropdown menu={{ items }} trigger={["click"]}>
-            <MenuOutlined />
-          </Dropdown>
-        </>
-      ) : (
-        <AppMenu />
-      )}
-      {changePasswordHolder}
-      {changeUserinfoHolder}
+      {isLogin ? <AppUserMenu /> : <AppPublicMenu />}
     </div>
   );
 };
