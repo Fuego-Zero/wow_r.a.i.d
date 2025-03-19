0f06cd2fdfbfaa2a4db55f4c51b5b712eb9243b4
Fuego <FuegoZero35@gmail.com>
2025-03-19 17:33:34 +0800

    新增 AppTitle 组件，包含 logo 和标题，添加 logo 图片

---
+++ web/src/app/components/AppTitle.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+
+function AppTitle(props: { title: string; subTitle: string }) {
+  return (
+    <>
+      <Image
+        src="/images/logo.jpg"
+        alt="logo"
+        width={50}
+        height={50}
+        className="mr-2"
+      />
+      <h1 className="flex items-end flex-1 truncate min-w-0 space-x-2">
+        <span className="text-base sm:text-lg md:text-xl lg:text-2xl text-amber-50">
+          {props.title}
+        </span>
+        <span className="text-base text-[12px]"> {props.subTitle}</span>
+      </h1>
+    </>
+  );
+}
+
+export default AppTitle;
