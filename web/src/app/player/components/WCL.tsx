5d6f8fe6077a4ff92a7fb01389aac371b4029aad
Fuego <FuegoZero35@gmail.com>
2025-03-21 16:22:24 +0800

    为 WCL 组件添加可选 mode 属性，优化布局适应性

--- web/src/app/player/components/WCL.tsx
+++ web/src/app/player/components/WCL.tsx
@@ -3,18 +3,22 @@ import { Tooltip } from "antd";
 import React, { memo } from "react";
 import { useAppConfig } from "../context/appConfigContext";
 import { getWClColor } from "@/app/utils";
+import classNames from "classnames";
 
 type Props = {
   talent: TalentType[];
   role_name: string;
+  mode?: "mobile" | "desktop";
 };
 
 function WCL(props: Props) {
-  const { talent, role_name } = props;
+  const { talent, role_name, mode = "desktop" } = props;
   const { WCLRanksMap } = useAppConfig();
 
+  const _className = mode === "desktop" ? "flex-col" : "flex-row text-[12px]";
+
   return (
-    <div className="flex flex-col">
+    <div className={classNames("flex", _className, mode)}>
       {talent.map((item) => {
         const key = role_name + item;
         const rank = WCLRanksMap.get(key);
