e72c06c5a473c474685fb02fb0b8d4d832f0800a
Fuego <FuegoZero35@gmail.com>
2025-03-22 10:03:49 +0800

    添加新图片并优化图片数组生成逻辑

--- web/src/app/photo/page.tsx
+++ web/src/app/photo/page.tsx
@@ -27,21 +27,9 @@ const ScrollWrap = dynamic(() => import("../components/common/ScrollWrap"), {
 });
 
 function PhotoContent() {
-  const images = [
-    "/images/0.jpg",
-    "/images/1.jpg",
-    "/images/2.jpg",
-    "/images/3.jpg",
-    "/images/4.jpg",
-    "/images/5.jpg",
-    "/images/6.jpg",
-    "/images/7.jpg",
-    "/images/8.jpg",
-    "/images/9.jpg",
-    "/images/10.jpg",
-    "/images/11.jpg",
-    "/images/12.jpg",
-  ];
+  const images = Array.from({ length: 26 }).map((_, index) => {
+    return `/images/${index}.jpg`;
+  });
 
   return (
     <Layout className="h-[100vh] flex overflow-auto">
