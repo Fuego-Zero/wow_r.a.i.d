5f216111a2e55ba47cbb3edb6ad719b6445fc09c
Fuego <FuegoZero35@gmail.com>
2025-03-22 21:36:51 +0800

    优化 PlayTime 组件，使用 useMemo 处理并排序报名时间，提高性能和可读性

--- web/src/app/components/PlayTime.tsx
+++ web/src/app/components/PlayTime.tsx
@@ -1,6 +1,6 @@
 import { CalendarFilled } from "@ant-design/icons";
 import { Tooltip } from "antd";
-import React from "react";
+import React, { useMemo } from "react";
 import { useAppConfig } from "../player/context/appConfigContext";
 import { UserInfo } from "../player/types";
 
@@ -9,7 +9,26 @@ function PlayTime(props: {
   className?: string;
 }) {
   const { play_time, className } = props;
-  const { raidTimeNameMap } = useAppConfig();
+  const { raidTimeNameMap, raidTimeOrderMap } = useAppConfig();
+
+  const playTime = useMemo(() => {
+    const formattedPlayTime = play_time.reduce((prev, time) => {
+      const timeName = raidTimeNameMap.get(time);
+      const timeOrder = raidTimeOrderMap.get(time);
+
+      if (!timeName || !timeOrder) return prev;
+
+      prev.push({
+        timeName,
+        timeOrder,
+      });
+
+      return prev;
+    }, [] as { timeName: string; timeOrder: number }[]);
+
+    formattedPlayTime.sort((a, b) => a.timeOrder - b.timeOrder);
+    return formattedPlayTime;
+  }, [play_time, raidTimeNameMap, raidTimeOrderMap]);
 
   return (
     <Tooltip
@@ -17,11 +36,9 @@ function PlayTime(props: {
       title={
         <>
           <div>报名时间：</div>
-          {play_time
-            .map((time) => raidTimeNameMap.get(time))
-            .map((time) => {
-              return <div key={time}>{time}</div>;
-            })}
+          {playTime.map((time) => {
+            return <div key={time.timeOrder}>{time.timeName}</div>;
+          })}
         </>
       }
     >
