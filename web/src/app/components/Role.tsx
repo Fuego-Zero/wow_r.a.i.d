c9564dfbea55cee0dde91e7fd5a94b5e275e0c1a
Fuego <FuegoZero35@gmail.com>
2025-03-24 11:29:14 +0800

    新增角色图像和 Role 组件，支持角色类型的显示

---
+++ web/src/app/components/Role.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React, { memo, useMemo } from "react";
+
+enum RoleMap {
+  TANK = "TANK",
+  DPS = "DPS",
+  HEAL = "HEAL",
+}
+
+type Props = {
+  role: keyof typeof RoleMap;
+  className?: string;
+  width?: number;
+  height?: number;
+};
+
+const IMAGES = {
+  TANK: "/images/roles/TANK.png",
+  DPS: "/images/roles/DPS.png",
+  HEAL: "/images/roles/HEAL.png",
+};
+
+function Role(props: Props) {
+  const { role, className, height = 24, width = 24 } = props;
+  const src = useMemo(() => IMAGES[role], [role]);
+
+  return (
+    <Image
+      className={className}
+      src={src}
+      width={width}
+      height={height}
+      alt="role"
+    />
+  );
+}
+
+export default memo(Role) as typeof Role;
