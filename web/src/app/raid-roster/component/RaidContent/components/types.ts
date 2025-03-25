a1cb0018b1deb8e8bc9c2edf5c5a6cbc963ff9a1
Fuego <FuegoZero35@gmail.com>
2025-03-25 17:02:23 +0800

    更新RaidPlayerCardsProps类型，使用PlayersData替代Player数组以增强类型安全

--- web/src/app/raid-roster/component/RaidContent/components/types.ts
+++ web/src/app/raid-roster/component/RaidContent/components/types.ts
@@ -1,12 +1,11 @@
-import { Handler, RaidData } from "@/app/raid-roster/types";
+import { Handler, PlayersData, RaidData } from "@/app/raid-roster/types";
 import { InferArrayItem } from "@yfsdk/web-basic-library";
 
 export type Data = InferArrayItem<RaidData>;
-export type Player = InferArrayItem<Data["players"]>;
 
 export type RaidCardProps = {
   data: Data;
   displayMode?: boolean;
 } & Partial<Handler>;
 
-export type RaidPlayerCardsProps = { players: Player[] } & RaidCardProps;
+export type RaidPlayerCardsProps = { players: PlayersData } & RaidCardProps;
