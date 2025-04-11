022335f23c801d1f500bacda3fbfa523177de9ce
Fuego <FuegoZero35@gmail.com>
2025-04-11 18:21:00 +0800

    优化 WCLRankingsService，添加角色和用户映射逻辑，确保返回的排名数据包含角色类别和用户名

--- server_node/src/interfaces/IWCLRankings.ts
+++ server_node/src/interfaces/IWCLRankings.ts
@@ -1,4 +1,9 @@
+import { IRole } from '../models/Role';
+import { IUser } from '../models/User';
 import { IWCLRankings } from '../models/WCLRankings';
 
-export interface IWCLRankingsResponse
-  extends Array<Pick<IWCLRankings, 'role_name' | 'talent' | 'average_rank_percent' | 'server_rank'>> {}
+type WCL = Pick<IWCLRankings, 'role_name' | 'talent' | 'average_rank_percent' | 'server_rank'>;
+type Role = Pick<IRole, 'classes'>;
+type User = Pick<IUser, 'user_name'>;
+
+export interface IWCLRankingsResponse extends Array<WCL & Role & User> {}
--- server_node/src/services/WCLRankingsService.ts
+++ server_node/src/services/WCLRankingsService.ts
@@ -1,15 +1,45 @@
+import { RoleClassesMap } from '../common';
 import { IWCLRankingsResponse } from '../interfaces/IWCLRankings';
+import Role from '../models/Role';
+import User from '../models/User';
 import WCLRankings from '../models/WCLRankings';
 
 class WCLRankingsService {
   static async getWCLRankings(): Promise<IWCLRankingsResponse> {
     const rankings = await WCLRankings.find({}).lean();
 
+    const roles = await Role.find({}).lean();
+    const rolesMap = roles.reduce(
+      (acc, role) => {
+        const { user_id, role_name, classes } = role;
+
+        acc[role_name] = {
+          user_id: user_id.toString(),
+          classes,
+        };
+
+        return acc;
+      },
+      {} as Record<string, { user_id: string; classes: RoleClassesMap }>,
+    );
+
+    const users = await User.find({}).lean();
+    const usersMap = users.reduce(
+      (acc, user) => {
+        const { _id, user_name } = user;
+        acc[_id.toString()] = user_name;
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
+
     return rankings.map((item) => ({
       role_name: item.role_name,
       talent: item.talent,
       average_rank_percent: item.average_rank_percent,
       server_rank: item.server_rank,
+      classes: rolesMap[item.role_name]?.classes,
+      user_name: usersMap[rolesMap[item.role_name]?.user_id] ?? '',
     }));
   }
 }
